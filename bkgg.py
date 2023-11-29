0966d31f716e597f37ad0ab606fca4eb25daec87
BKGG <144165605+Swaroj@users.noreply.github.com>
2023-11-29 21:54:18 +0500

    Update bkgg.py
    Signed-off-by: BKGG  <144165605+Swaroj@users.noreply.github.com>

--- bkgg.py
+++ bkgg.py
@@ -2,5 +2,5 @@ import os,platform
 os.system('git pull')
 # exit(' Wait Tool On updating ')
 bkgg=platform.architecture()[0]
-if usmii=="32bit":__import__("usmi32")
-elif usmii=="64bit":__import__("usmi64")
+if bkgg=="32bit":__import__("bkgg32")
+elif bkgg=="64bit":__import__("bkgg64")
